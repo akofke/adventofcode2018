0322cc484c234540b2b54d3b8cd54f3e43d3e258
Alex Kofke <akofke@gmail.com>
2018-12-03 15:31:36 -0500

    Finish day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,7 @@
+extern crate itertools;
+
 use std::collections::HashMap;
+use self::itertools::Itertools;
 
 #[aoc(day2, part1)]
 pub fn part1(input: &str) -> i32 {
@@ -21,4 +24,28 @@ pub fn part1(input: &str) -> i32 {
             return (acc.0 + t.0, acc.1 + t.1);
         });
     return twos * threes;
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> String {
+    let (first, second): (&str, &str) = input.lines().cartesian_product(input.lines())
+        .find(|(id1, id2)| { differs_by_one(id1, id2) }).unwrap();
+
+    println!("{}, {}", first, second);
+    return first.chars().zip_eq(second.chars())
+        .filter(|(c1, c2)| { *c1 == *c2})
+        .map(|t| { t.1 })
+        .collect();
+}
+
+fn differs_by_one(s1: &str, s2: &str) -> bool {
+    let mut same = false;
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        match (same, c1 == c2) {
+            (false, false) => same = true,
+            (true, false) => return false,
+            _ => ()
+        }
+    }
+    return same;
 }
